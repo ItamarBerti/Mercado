cee18fc1f30d0dc13c048d8fe795a1120b8fd4f5
ItamarBerti <101076577+ItamarBerti@users.noreply.github.com>
2022-06-22 11:43:58 -0300

    Att

--- ConsoleApp42/Menu.cs
+++ ConsoleApp42/Menu.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace ConsoleApp42
 {
@@ -11,25 +10,25 @@ namespace ConsoleApp42
             List<Produto> produtos = new List<Produto>();
             List<Vendedor> vendedores = new List<Vendedor>();
             List<Cliente> clientes = new List<Cliente>();
-            List<Venda> vendas = new List<Venda>();           
+            List<Venda> vendas = new List<Venda>();
             Venda venda = new Venda();
-            
-            
+
+
 
 
             do
-            {             
+            {
                 int opcao = 0;
 
                 Console.WriteLine("[1]- cadastrar Cliente");
-                Console.WriteLine("[2]- cadastrar Vendedor"); 
+                Console.WriteLine("[2]- cadastrar Vendedor");
                 Console.WriteLine("[3]- cadastrar Produto");
                 Console.WriteLine("[4]- inserir um produto no carrinho de compra");
                 Console.WriteLine("[5]- finalizar a Venda");
                 Console.WriteLine("[6]- limpar o carrinho");
                 Console.WriteLine("[7]- Sair");
                 opcao = Convert.ToInt32(Console.ReadLine());
-                if(opcao == 8)
+                if (opcao == 8)
                 {
                     Console.WriteLine("Obrigado por usar o nosso sistema! ");
                     break;
@@ -47,7 +46,7 @@ namespace ConsoleApp42
                         cliente.cpf = Console.ReadLine();
                         Console.WriteLine("Digite o Endereço: ");
                         cliente.endereco = Console.ReadLine();
-                        clientes.Add(cliente);                                           
+                        clientes.Add(cliente);
                         break;
                     case 2:
                         Console.WriteLine("Cadastrando o vendedor... ");
@@ -56,7 +55,7 @@ namespace ConsoleApp42
                         vendedor.codigoCracha = Console.ReadLine();
                         Console.WriteLine("Digite o nome do vendedor: ");
                         vendedor.nome = Console.ReadLine();
-                        vendedores.Add(vendedor);                                            
+                        vendedores.Add(vendedor);
                         break;
                     case 3:
                         Console.WriteLine("Cadastrando o produto... ");
@@ -72,12 +71,12 @@ namespace ConsoleApp42
                     case 4:
                         Console.WriteLine("Insira seu produto ao seu carrinho... ");
                         Menu.MostrarClientesCadastrados(clientes);
-                        Console.WriteLine("Digite qual cliente você é: ");                    
+                        Console.WriteLine("Digite qual cliente você é: ");
                         cliente = Menu.ProcurarClientePorId(clientes, Convert.ToInt32(Console.ReadLine()));
-                        Menu.MostrarListaDeProdutos(produtos);                    
+                        Menu.MostrarListaDeProdutos(produtos);
                         Console.WriteLine("Qual produtos você deseja inserir no carrinho? ");
                         produto = Menu.ProcurarProdutoPorId(produtos, Convert.ToInt32(Console.ReadLine()));
-                        cliente.carrinho.produtos.Add(produto);           
+                        cliente.carrinho.produtos.Add(produto);
                         break;
                     case 5:
                         Console.WriteLine("Finalizando a sua venda... ");
@@ -86,10 +85,10 @@ namespace ConsoleApp42
                         vendedor = Menu.ProcurarVendedorPorId(vendedores, Convert.ToInt32(Console.ReadLine()));
                         Console.WriteLine();
                         Menu.MostrarClientesCadastrados(clientes);
-                       Console.WriteLine("Digite qual cliente você quer inicar a venda: ");                    
-                        cliente = Menu.ProcurarClientePorId(clientes, Convert.ToInt32(Console.ReadLine()));   
-                        Console.WriteLine("O valor total da sua compra é: " + cliente.carrinho.ValorTotalCarrinho());                       
-                        break;            
+                        Console.WriteLine("Digite qual cliente você quer inicar a venda: ");
+                        cliente = Menu.ProcurarClientePorId(clientes, Convert.ToInt32(Console.ReadLine()));
+                        Console.WriteLine("O valor total da sua compra é: " + cliente.carrinho.ValorTotalCarrinho());
+                        break;
                     case 6:
                         Console.WriteLine("Limpando o seu carrinho... ");
                         Menu.MostrarClientesCadastrados(clientes);
@@ -105,9 +104,9 @@ namespace ConsoleApp42
 
         public static void MostrarListaDeProdutos(List<Produto> produtos)
         {
-           foreach(Produto produto in produtos)
+            foreach (Produto produto in produtos)
             {
-                Console.WriteLine("ID: " + produto.id + " - Descrição do produto: " + produto.descricaoProduto + " - Preço: " + produto.preco);           
+                Console.WriteLine("ID: " + produto.id + " - Descrição do produto: " + produto.descricaoProduto + " - Preço: " + produto.preco);
             }
         }
         public static void MostrarListaDeVendedores(List<Vendedor> vendedores)
@@ -126,25 +125,25 @@ namespace ConsoleApp42
         }
         public static Cliente ProcurarClientePorId(List<Cliente> clientes, int id)
         {
-            for(int i = 0; i < clientes.Count; i++)
+            for (int i = 0; i < clientes.Count; i++)
             {
-                if(clientes[i].id == id) return clientes[i];
+                if (clientes[i].id == id) return clientes[i];
             }
             return null;
         }
         public static Produto ProcurarProdutoPorId(List<Produto> produtos, int id)
         {
-            for(int i = 0; i < produtos.Count; i++)
+            for (int i = 0; i < produtos.Count; i++)
             {
-                if(produtos[i].id == id) return produtos[i];
+                if (produtos[i].id == id) return produtos[i];
             }
             return null;
         }
         public static Vendedor ProcurarVendedorPorId(List<Vendedor> vendedores, int id)
         {
-            for(int i = 0; i < vendedores.Count; i++)
+            for (int i = 0; i < vendedores.Count; i++)
             {
-                if(vendedores[i].id == id) return vendedores[i];
+                if (vendedores[i].id == id) return vendedores[i];
             }
             return null;
         }
